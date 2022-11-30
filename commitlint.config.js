503971076fb342a78c6789cda5be35785bda0a69
coderQls <1064341174@qq.com>
2022-11-30 17:23:24 +0800

    feat: 添加 commitlint + husky

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: coderQls 1064341174@qq.com
+ * @Date: 2022-11-30 16:42:29
+ * @LastEditors: coderQls 1064341174@qq.com
+ * @LastEditTime: 2022-11-30 17:02:04
+ * @Description:
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+};
